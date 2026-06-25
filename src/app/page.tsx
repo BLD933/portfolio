a2da5198433bc57b24809ab1006c49bbcb8a4c0d
BLD933 <bouloud94@gmail.com>
2026-06-25 03:34:40 +0100

    fix: convert page to client component for static export hydration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { FC } from "react";
 import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
 import SkillsSection from "@/components/SkillsSection";
@@ -5,7 +8,7 @@ import ExperienceSection from "@/components/ExperienceSection";
 import ProjectsSection from "@/components/ProjectsSection";
 import ContactSection from "@/components/ContactSection";
 
-export default function Home() {
+const Home: FC = () => {
   return (
     <>
       <HeroSection />
@@ -16,4 +19,6 @@ export default function Home() {
       <ContactSection />
     </>
   );
-}
+};
+
+export default Home;
